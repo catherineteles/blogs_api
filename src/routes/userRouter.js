3a6bdaff333127ca353890fda3b449881fdb1d8f
cathe <cacateles12@gmail.com>
2022-07-15 11:33:09 -0300

    Added endpoint to get all users after tokenvalidation

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
 
 const usersController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = Router();
 
 router.post('/', usersController.create);
+
+router.use(authController.validateToken);
+
+router.get('/', usersController.list);
 
 module.exports = router;
--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -1,4 +1,4 @@
-require('dotenv/config');
+require('dotenv');
 const jwt = require('jsonwebtoken');
 
 const jwtService = {
